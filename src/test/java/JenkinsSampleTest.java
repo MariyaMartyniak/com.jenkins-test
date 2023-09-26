17de5edc94925fa03582626cce69234a7c80f77a
mmartyniak <mmartyniak@insperex.com>
2023-09-26 13:45:28 +0300

    updated build.gradle

--- src/test/java/JenkinsSampleTest.java
+++ src/test/java/JenkinsSampleTest.java
@@ -1,5 +1,9 @@
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -7,9 +11,19 @@ import static com.codeborne.selenide.Selenide.$x;
 
 class JenkinsSampleTest {
 
+    @BeforeAll
+    public void setUp() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+
     @Test
     void verifyBasicJenkinsTest() {
         Selenide.open("https://www.google.com/");
         $x("//title[text()='Google']").shouldBe(Condition.visible);
+    }
+
+    @AfterAll
+    public void tearDown() {
+        SelenideLogger.removeListener("allure");
     }
 }
